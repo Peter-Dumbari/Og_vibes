3203be6f38da389579043b020948a3ff2afc3047
Peter Dumbari <dummytwiz@gmail.com>
2024-11-25 18:09:54 +0100

    design an html for blog Main Card

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Wrapper from "../components/ui/wrapper";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="blogs_container">
+      <div className="banner">
+        <h3>Your Daily Dose of Knowledge and Inspiration</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          possimus ipsam optio, veniam quas alias dignissimos blanditiis aliquam
+          sed quibusdam.
+        </p>
+      </div>
+
+      <Wrapper>
+        <div className="blogs_holder"></div>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Blogs;
