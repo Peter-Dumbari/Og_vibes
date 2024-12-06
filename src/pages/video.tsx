f4f0848adf7c5bc0fa2afb3d3dd57151ca7618b0
Peter Dumbari <dummytwiz@gmail.com>
2024-12-06 17:37:09 +0100

    adjust media query for blog and other page

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -4,6 +4,50 @@ import { VideoCard } from "../components/ui/card";
 import Wrapper from "../components/ui/wrapper";
 
 const Video = () => {
+  const demoVideo = [
+    {
+      thumbnail:
+        "https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg",
+      title: "Person wey go help you no go stress you - Asake",
+      isPlaying: true,
+    },
+    {
+      thumbnail:
+        "https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg",
+      title: "Lonely at the top",
+      isPlaying: false,
+    },
+    {
+      thumbnail:
+        "https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg",
+      title: "Lonely at the top",
+      isPlaying: false,
+    },
+    {
+      thumbnail:
+        "https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg",
+      title: "Lonely at the top",
+      isPlaying: false,
+    },
+    {
+      thumbnail:
+        "https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg",
+      title: "Lonely at the top",
+      isPlaying: false,
+    },
+  ];
+
+  const [currentPage, setCurrentPage] = React.useState<number>(1);
+  const videoPerPage = 6;
+  const displayedPages = Math.ceil(demoVideo.length / videoPerPage);
+  const index = (currentPage - 1) * videoPerPage;
+  const currentVideos = demoVideo.slice(index, currentPage + videoPerPage);
+
+  const handleChange = (page: number) => {
+    if (page >= 1 && page <= displayedPages) {
+      setCurrentPage(page);
+    }
+  };
   return (
     <div className="video_cont">
       <div className="banner">
@@ -17,17 +61,25 @@ const Video = () => {
 
       <Wrapper>
         <div className="videos">
-          <div className="all_vid">
-            <VideoCard
-              thumbnail="https://trendybeatz.com/images/Asake-Work-Of-Art-AlbumArtwork1.jpg"
-              title="Lonely at the Top - Asake"
-              isPlaying
-            />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+          <div className="vids_container">
+            <div className="all_vid">
+              {currentVideos.map((item, idx) => {
+                return (
+                  <VideoCard
+                    key={idx}
+                    thumbnail={item.thumbnail}
+                    title={item.title}
+                    isPlaying={item.isPlaying}
+                  />
+                );
+              })}
+            </div>
           </div>
-          <PaginationComponent />
+          <PaginationComponent
+            onPageChange={handleChange}
+            totalPages={displayedPages}
+            currentPage={currentPage}
+          />
         </div>
       </Wrapper>
     </div>
