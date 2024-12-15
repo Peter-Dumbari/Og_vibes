9420886d2e9cf617200e8a4bec5b87107d3080cf
Peter Dumbari <dummytwiz@gmail.com>
2024-12-15 19:43:05 +0100

    styleless design of contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,5 @@
+import { MapPinIcon } from "@heroicons/react/16/solid";
+import { EnvelopeOpenIcon, PhoneIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 const Contact = () => {
@@ -11,6 +13,36 @@ const Contact = () => {
           sed quibusdam.
         </p>
       </div>
+
+      <section className="contacts_means">
+        <h3>Get in Touch</h3>
+        <div className="inner">
+          <div className="office_Add">
+            <MapPinIcon className="icon" />
+          </div>
+          <div className="phone">
+            <PhoneIcon className="icon" />
+          </div>
+          <div className="email">
+            <EnvelopeOpenIcon className="icon" />
+          </div>
+        </div>
+
+        <div className="form">
+          <h3>Send Us a Message</h3>
+          <form action="">
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <textarea
+              name=""
+              id=""
+              cols={30}
+              rows={10}
+              placeholder="Message"></textarea>
+            <button>Send</button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
