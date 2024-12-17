03596e3bce825e0f5ac6cb718f9ab7f20e8dbeff
Peter Dumbari <dummytwiz@gmail.com>
2024-12-17 14:17:06 +0100

    initial commit for loader

---
+++ src/components/ui/loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader_cont">
+      <div className="inner">
+        <div className="loader_main">
+          <div className="loader"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
